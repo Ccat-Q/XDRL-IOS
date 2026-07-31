8a0a0b0a5d8db09ee22c899349f2e7c0ca713b3b
Ccat_Q <16045902+ccat_q@user.noreply.gitee.com>
2026-07-31 18:37:23 +0800

    Fix Gradle repo conflict: PREFER_SETTINGS + Compose repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,10 +6,11 @@
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 rootProject.name = "xdyl"
