4b03fbd8ccd1a933f960348e41bb52693012f313
Ccat_Q <16045902+ccat_q@user.noreply.gitee.com>
2026-08-01 23:36:38 +0800

    Add border import

--- shared/src/commonMain/kotlin/www/xdyl/hygge/shared/ExtensionScreen.kt
+++ shared/src/commonMain/kotlin/www/xdyl/hygge/shared/ExtensionScreen.kt
@@ -1,6 +1,7 @@
 package www.xdyl.hygge.shared
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
